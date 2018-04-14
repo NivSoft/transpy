e39687b05c383bf281b2743bd40c361b902f3eae
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-14 19:15:43 -0300

    Atualização de diretórios

--- licitacao/views.py
+++ licitacao/views.py
@@ -8,4 +8,4 @@ from licitacao.models import Modalidade
 def licitacao(request):
     lista_modalidades = Modalidade.objects.all().filter(ano=now().year).order_by('numero')
     context = RequestContext(request)
-    return render(request, 'licitacao/home.html')
+    return render(request, 'licitacao/home.html',{'lista':lista_modalidades})
