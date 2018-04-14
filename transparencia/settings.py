906761170dbed47de867b1031243ad90f6f32fdb
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-14 18:56:28 -0300

    Atualização de diretórios

--- transparencia/settings.py
+++ transparencia/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/../media')
 MEDIA_URL = '/media/'
--- transparencia/urls.py
+++ transparencia/urls.py
@@ -23,7 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('licitacao.urls')),
     path('', include('administrativo.urls')),
-]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
