83e3fcdf09b07424dd7fa28f4e1fcf2d67d56668
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-14 18:45:42 -0300

    Atualização de diretórios

--- transparencia/settings.py
+++ transparencia/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_URL = '/media/'
