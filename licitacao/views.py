2996739412742f50b2ce270757086e3c21a4aa76
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-15 10:54:03 -0300

    Atualização

--- licitacao/views.py
+++ licitacao/views.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 from licitacao.models import Modalidade
 
 def licitacao(request):
-    ano_atual = now()
-    lista_modalidades = Modalidade.objects.all().filter(ano=ano_atual.year).order_by('criado_em')
+    ano_atual = now().year
+    lista_modalidades = Modalidade.objects.all().filter(ano=ano_atual).order_by('criado_em')
     context = RequestContext(request)
     return render(request, 'licitacao/home.html',{'lista':lista_modalidades,'ano':ano_atual})
