a31e56ff570fdef5b7d25dfaee7b2010ff8fdfea
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-15 18:06:52 -0300

    Atualização

--- licitacao/views.py
+++ licitacao/views.py
@@ -5,11 +5,13 @@ from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from django.utils.timezone import now
 
-from licitacao.models import Modalidade, Tipo, Ano, Situacao
+from licitacao.models import Modalidade, Tipo, Situacao
 
 
 def licitacao(request):
     ano_atual = now().year
+    modalidades = Tipo.objects.all().order_by('nome')
+    status = Situacao.objects.all().order_by('status')
     lista_modalidades = Modalidade.objects.all().filter(ano=ano_atual).order_by('-criado_em')
     query = request.GET.get("q")
     if query:
@@ -28,5 +30,7 @@ def licitacao(request):
         'ano':ano_atual,
         'contacts': contacts,
         'pagina_request':pagina_request,
+        'modalidades':modalidades,
+        'status':status
         }
     return render(request, 'licitacao/home.html',context)
