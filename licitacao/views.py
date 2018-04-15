25517e0e849ef8dd3fb843e7e4769a54670d1e3e
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-04-15 11:53:34 -0300

    Atualização

--- licitacao/views.py
+++ licitacao/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from django.utils.timezone import now
 
-from licitacao.models import Modalidade
+from licitacao.models import Modalidade, Tipo
+
 
 def licitacao(request):
     ano_atual = now().year
     lista_modalidades = Modalidade.objects.all().filter(ano=ano_atual).order_by('criado_em')
+    modalidades = Tipo.objects.all().order_by('nome')
     context = RequestContext(request)
-    return render(request, 'licitacao/home.html',{'lista':lista_modalidades,'ano':ano_atual})
+    return render(request, 'licitacao/home.html',{'lista':lista_modalidades,'ano':ano_atual,'modalidades':modalidades})
 
 def filtros(request, modalidade_tipo,modalidade_ano, modalidade_situacao, modalidade_objeto):
     modalidade = Modalidade.objects.all().filter(
