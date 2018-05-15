94a1884ffbeb0e2e2734973772795ab16247a116
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-05-15 00:56:56 -0300

    Atualização do model licitação

--- licitacao/models.py
+++ licitacao/models.py
@@ -105,7 +105,7 @@ class Modalidade(models.Model):
     tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
     numero = models.PositiveIntegerField(default=None, blank=True, null=True)
     processo = models.ForeignKey(Processo, on_delete=models.CASCADE, blank=True, null=True)
-    contrato = models.CharField(default=None, max_length=10)
+    contrato = models.CharField(default=None, max_length=10, blank=True, null=True)
     objeto = models.TextField()
     situacao = models.ForeignKey(Situacao, on_delete=models.CASCADE)
     ano = models.PositiveIntegerField(default=now().year, blank=True, null=True)
