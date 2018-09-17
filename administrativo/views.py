65ab646aa063cdf94fe62199dab3612f2e73d02d
Danilo Rodrigues <danilorodrigues@nivsoft.com.br>
2018-09-17 12:48:44 -0300

    Constução do administrativo

--- administrativo/views.py
+++ administrativo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.template import RequestContext
 
+from administrativo.models import Orgao
+
 
 def home(request):
-    context = RequestContext(request)
-    return render(request, 'administrativo/home.html', {})
+    dados = Orgao.objects.all()
+    context = {
+
+        'dados':dados,
+
+    }
+    return render(request, 'administrativo/home.html', context)
